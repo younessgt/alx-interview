df6d8213972a4c6a794b55f66dda13b21ee6149c
younessgt <youness.merzak123@gmail.com>
2024-01-25 16:42:29 +0100

    task0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -46,7 +46,7 @@ if __name__ == "__main__":
             else:
                 continue
 
-    except KeyboardInterrupt:
+    except Exception:
         print_stats()
     # finally:
         # print_stats()
