969f2892fae4c5983e44fc115d49e4ef7d68e193
younessgt <youness.merzak123@gmail.com>
2024-01-25 16:54:02 +0100

    task0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -34,11 +34,15 @@ if __name__ == "__main__":
                     total_file_size += int(file_size)
 
                 if match_status:
-                    status = match_status.group(1)
-                    if status in dict_status:
-                        dict_status[status] += 1
-                    else:
-                        dict_status[status] = 1
+                    try:
+                        status = int(match_status.group(1))
+
+                        if status in dict_status:
+                            dict_status[status] += 1
+                        else:
+                            dict_status[status] = 1
+                    except Exception:
+                        pass
 
                 if count % 10 == 0:
                     print_stats()
