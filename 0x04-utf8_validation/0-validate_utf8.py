4e6fe3c76403f826450b0cfef61b067f3fd1392d
younessgt <youness.merzak123@gmail.com>
2024-01-30 13:25:36 +0100

    validation Utf-8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" script contain validUTF8 function"""
+
+
+def validUTF8(data):
+    """method that determines if a given data
+    set represents a valid UTF-8 encoding"""
+
+    try:
+        _ = bytearray(data).decode('utf-8')
+        return True
+    except Exception:
+        return False
