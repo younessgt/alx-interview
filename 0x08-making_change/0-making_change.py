0591757629f73fd9b07546b6cf7c37cb7ba769b0
younessgt <youness.merzak123@gmail.com>
2024-02-29 16:16:24 +0100

    fix efficiency

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,11 +10,21 @@ def makeChange(coins, total):
     acc = [total + 1] * (total + 1)
     acc[0] = 0
 
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i >= coin:
-                j = i - coin
-                acc[i] = min(acc[j] + 1, acc[i])
+    # for i in range(1, total + 1):
+    # for coin in coins:
+    # if i >= coin:
+    # j = i - coin
+    # acc[i] = min(acc[j] + 1, acc[i])
+
+    # more efficient way is to iterate first throught the coins
+    # in that way it ensure that each i in the inner loop
+    # is at least as large as the coin being considered
+    # better than checking every time if i >= coin
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            acc[i] = min(acc[i - coin] + 1, acc[i])
+
     if acc[total] == total + 1:
         return -1
     else:
