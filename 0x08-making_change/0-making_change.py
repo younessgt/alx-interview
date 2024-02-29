2bd20f2959a11e0b277a7ec1ff06f97a8775e128
younessgt <youness.merzak123@gmail.com>
2024-02-29 16:30:09 +0100

    method 2

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,26 +7,38 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    coins = [coin for coin in coins if coin <= total]
-    acc = [total + 1] * (total + 1)
-    acc[0] = 0
+    # acc = [total + 1] * (total + 1)
+    # acc[0] = 0
+    acc = []
 
-    # for i in range(1, total + 1):
-    # for coin in coins:
-    # if i >= coin:
-    # j = i - coin
-    # acc[i] = min(acc[j] + 1, acc[i])
+    """for i in range(1, total + 1):
+        for coin in coins:
+            if i >= coin:
+            j = i - coin
+            acc[i] = min(acc[j] + 1, acc[i]) """
 
     # more efficient way is to iterate first throught the coins
     # in that way it ensure that each i in the inner loop
     # is at least as large as the coin being considered
     # better than checking every time if i >= coin
 
-    for coin in coins:
+    """"for coin in coins:
         for i in range(coin, total + 1):
             acc[i] = min(acc[i - coin] + 1, acc[i])
 
     if acc[total] == total + 1:
         return -1
     else:
-        return acc[total]
+        return acc[total]"""
+
+    # method 2
+    coins.sort()
+    i = len(coins) - 1
+    while (i >= 0):
+        while (total >= coins[i]):
+            total -= coins[i]
+            acc.append(coins[i])
+        i -= 1
+    if total != 0:
+        return -1
+    return len(acc)
